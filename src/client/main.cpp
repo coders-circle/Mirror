402f5de9d33e78bd222615ec7f28891a976b3ae7
Bibek Pandey <spirit_bibek@yahoo.com>
2014-12-08 22:17:44 +0545

    added a couple of lines to test

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -5,13 +5,17 @@
 //boost test for separate thread
 void Thread()
 {
-    while (true) 
-        std::cout << "From Child Thread" << std::endl;
+		int i=0;
+		while(i<100)
+		{
+      	std::cout << "From Child Thread" << std::endl;
+			i++;
+		}
 }
 int main()
 {
     boost::thread t(Thread);
-    while (true)
-        std::cout << "From Main Thread" << std::endl;
+    std::cout << "From Main Thread" << std::endl;
+	 t.join();
     return 0;
 }
