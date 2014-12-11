bd6ada0f0a4ef333f822066f9de51f70d22c10e5
Bibek Dahal <bibekdahal_bd16@hotmail.com>
2014-12-11 19:47:39 +0545

    Add TcpClient

--- include/client/TcpClient.h
+++ include/client/TcpClient.h
@@ -1,12 +1,17 @@
 #pragma once
+#include <common/TcpListener.h>
+
 class TcpClient
 {
 public:
     TcpClient(boost::asio::io_service &io);
     ~TcpClient();
 
-    void StartListening();
+    void StartListening(const tcp::endpoint& server);
 
 private:
     boost::asio::io_service &m_io;
+    TcpListener m_listener;
+
+    void ListenerHandler(boost::shared_ptr<tcp::socket> socket);
 };
--- src/client/TcpClient.cpp
+++ src/client/TcpClient.cpp
@@ -2,9 +2,21 @@
 #include <client/TcpClient.h>
 
 TcpClient::TcpClient(boost::asio::io_service &io)
-:m_io(io)
+:m_io(io), m_listener(io)
 {}
 
 TcpClient::~TcpClient()
 {}
+
+void TcpClient::StartListening(const tcp::endpoint& server)
+{
+    m_listener.Initialize(server);
+    m_listener.Listen(boost::bind(&TcpClient::ListenerHandler, this, _1));
+}
+
+
+void TcpClient::ListenerHandler(boost::shared_ptr<tcp::socket> socket)
+{
+
+}
 
