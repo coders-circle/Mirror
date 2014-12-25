599dd15bf8beae9cbe61e708cdff698b018f687a
frozenhelium <fren.ankit@gmail.com>
2014-12-25 21:13:33 +0545

    okie what is this!!! zzz

---
+++ include/client/UI/Control.h
@@ -0,0 +1,31 @@
+#ifndef __CONTROL__
+#define __CONTROL__
+
+#include <gtk/gtk.h>
+#include <string>
+
+enum CONTROL{ BUTTON, LABEL, TEXTEDIT, STATUSBAR};
+
+// base class for UI control elements
+class Control
+{
+public:
+    
+    Control() :m_handle(0){}
+    void SetID(int id);
+    int GetID();
+    void PutFixedAt(GtkWidget *fixed, int x, int y);
+    void PutFixedRelativeTo(Control* obj, GtkWidget* fixed, int xOffset, int yOffset);
+    void Show();
+    void Hide();
+    GtkWidget* GetHandle();
+    // void SetEventHandler();
+protected:
+    GtkWidget* m_handle;
+    int m_x, m_y, m_w, m_h;
+    std::string m_label;
+    int m_type;
+    int m_id;
+};
+
+#endif
--- src/common/Control.cpp
+++
@@ -1,6 +0,0 @@
-#include "common/Control.h"
-
-void Control::PutFixedAt(GtkWidget *fixed, int x, int y)
-{
-
-}
