564d8666edecbec018d606f9884b53ed9539ae02
NISH1001 <nish_play1001@yahoo.com>
2014-12-27 18:15:30 +0545

    raw audio file

---
+++ include/client/Audio/AudioBuffer.h
@@ -0,0 +1,51 @@
+#ifndef AUDIO_BUFFER_H
+#define AUDIO_BUFFER_H
+
+#include <vector>
+#include <iterator>
+#include <iostream>
+#include <fstream>
+
+#include <portaudiocpp/PortAudioCpp.hxx>
+
+/*
+	This is the class for holding everything about our audio.
+
+
+
+*/
+
+class AudioBuffer
+{
+public:
+	AudioBuffer(int sizeHint);
+	~AudioBuffer(void);
+
+	/* The callback functoin for recording  */
+	int RecordCallback(const void *inputBuffer,
+						void * outputBuffer,
+						unsigned long framesPerBuffer,
+						const PaStreamCallbackTimeInfo* timeInfo,
+						PaStreamCallbackFlags statusFlags);
+
+	/* callback function for playing back the recorded data*/
+	int PlaybackCallback(const void *inputBuffer,
+						void * outputBuffer,
+						unsigned long framesPerBuffer,
+						const PaStreamCallbackTimeInfo* timeInfo,
+						PaStreamCallbackFlags statusFlags);
+
+	/* clears the input buffer before starting new recording*/
+	void ClearBuffer(void);
+
+	/* resets the buffer playback */
+	void ResetPlayback(void);
+
+	void WriteToFile(const std::string & strFilename);
+
+private:
+	std::vector<short> m_vectorSamples;
+	std::vector<short>::iterator m_iterPlayback;
+};
+
+#endif
