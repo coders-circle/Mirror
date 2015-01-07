492f5f43a7dce32b6fe178f8e1f404a4c13861db
Bibek Pandey <spirit_bibek@yahoo.com>
2015-01-07 17:11:27 +0545

    simple login and signup now works via JSON

--- include/common/JSONHandler.h
+++ include/common/JSONHandler.h
@@ -1,14 +1,20 @@
 #pragma once
-#include "rapidjson/docoument.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
+
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+#include <common/common.h>
+
+
+using namespace rapidjson;
 
 // the request action codes
-enum class REQUEST
+enum class REQUEST 
 {
 	EMPTY, // for notifying the server online status
 	SIGNUP,
 	LOGIN,
+	GET_SERVER_LIST,
 	JOIN_SERVER,
 	QUERY_FRIENDS,
 	QUERY_ONLINE_FRIENDS,
@@ -24,13 +30,24 @@ enum class REQUEST
 	// NOTE : when to obtain int value, static cast is to be used
 };
 
+// error codes
+enum class ERROR
+{
+	ALL_OKAY,
+	LOGIN_ERROR,
+	SIGNUP_ERROR,
+	INVALID_REQUEST,
+	JSON_FIELD_ERROR,
+	SERVER_ERROR,
+	// USAGE : ERROR_CODE error = ERROR_CODE::LOGIN_ERROR
+	// static cast for retrieving int value
+};
+
 // the response action codes
 enum class RESPONSE
 {
 	// to be added
 };
-
-
 
 class JSONHandlerException : public Exception
 {
@@ -40,49 +57,42 @@ class JSONHandlerException : public Exception
 	{}
 };
 
+
 class JSONHandler
 {
 	public:
 		JSONHandler();
 		~JSONHandler();
 		
-		void Login(std::string username, std::string password);
-		//void QueryFriends();
-		//void QueryOnlineFriends();
+		// SetRequest() is a function that sets basic json fields
+		void SetRequest(REQUEST);
+		void SetRequest(REQUEST, std::string user);
+		// signup request creator
+		void SetSignupRequest(std::string, std::string, std::string, std::string, std::string, std::string, std::string);
+		// login request creator
+		void SetLoginRequest(std::string username, std::string password);
+		// joinserver request creater
+		void SetJoinServerRequest(std::string serveName);
+		// sendMessage request creator
+		void SetSendMessageRequest(std::string username, std::string message);
+		// return the string form of the json request
+		std::string GetRequest();
 
-		void CreateRequest(int actionCode);
-		void CreateRequest(int actionCode, std::string user);
-		/*void SendFriendRequest(std::string friendName);
-		void SearchPeople(std::string name);
-		void GetUserDetail(std::string name);
-		void JoinServer(std::string serveName);
-		*/
+		
+		// get the response string and parse it
+		void ParseResponse(std::string);
 
-		void SignupRequest(/*Signup data */);
-
-		/*void SendCallRequest(std::string username);
-		void AcceptFriend(std::string username);
-		void ReceiveOffMessages();
-		void ReceiveFriendRequests();
-		*/
-		void SendMessageRequest(std::string username, std::string message);
-/*
-	StringBuffer buffer;
-    Writer<StringBuffer> writer(buffer);
-    m_document.Accept(writer);
-    return buffer.GetString();
-*/
 	private:
 		// attributes
-		rapidjson::Document m_request;
-		rapidjson::Document m_rsponse;
-		std::string m_usename;
+		Document m_request;
+		Document m_response;
+		std::string m_username;
 		std::string m_sessionId;
 
 		// shortcut for creating new json object
 		inline void New()
 		{
-			m_request = rapidjson::Document();
+			m_request = Document();
 			m_request.SetObject();
 		}
 
